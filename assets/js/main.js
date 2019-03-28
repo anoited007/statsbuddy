5ac6d76993657b256d1ec10af2983f7f6bb48802
anoited007 <esackey15@alustudent.com>
2019-03-29 00:55:46 +0400

    Get country code bug fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,21 +14,20 @@ Declaring Global Variables. These are Global because they will be accessed in di
 Also, they don't require the DOM to be ready before they can be initialiazed.
 */
 let baseUrl = "https://api.worldbank.org/v2/";
-var countries = {"ghana":"GHA","mauritius":"MUS", "united-kingdom":"GBR" };
+
+//var countries = {"ghana":"GHA","mauritius":"MUS", "united-kingdom":"GBR" };
 var indicators = {"internet-penetration":"IT.NET.USER.P3","cellular-subscription":"IT.CEL.SETS.P2",
                   "fixed-broadband":"IT.NET.BBND.P2","secure-server":"IT.NET.SECR.P6", "personal-computers":"IT.CMP.PCMP.P2", "population":"SP.POP.TOTL", "population-density":"EN.POP.DNST",
                  "gdp":"NY.GDP.MKTP.CD", "gdp-percapita":"NY.GDP.PCAP.PP.KD"};
 
-var country_arr = ["ghana"];
-var country_codes = [];
+var country_codes = ["GHA"];
 
 //equivalent to $(document).ready()
 $(function(){
-
+  var countries = $("input[type=checkbox]");
   var canvas = $("#chart");
 
    // TODO: if country is more than 1 add ';' in between.
-  var country = getCountryCode();
   var indicator = "IT.NET.USER.P3";
 
   $("input[type=radio]").each(function() {
@@ -36,38 +35,27 @@ $(function(){
   })
 
   $("input[type=checkbox]").each(function() {
-    $(this).on("change",getSelectedCountry);
+    $(this).on("change",getCountry);
   })
 
 
-})
-
-
-  function getCountry(){
-    if($.inArray(this.value, country_arr) === -1 && this.checked){
-      country_arr.push(this.value);
-    }
-
-    else if ($.inArray(this.value, country_arr) >= 0 && !this.checked) {
-      country_arr.pop(this.value);
-    }
-    console.log("This is country " +country_arr);
-    getCountryCode();
-    return country_arr;
-  }
-
-  function getSelectedCountry() {
-    $("input[type=checkbox]").each(function () {
-      if(this.checked){
-        let code = $(this).attr("data-code") //equivalent to this.getAttribute("data-code")
-        country_codes.push(code)
+    function getCountry(){
+      if($.inArray($(this).attr("data-code"), country_codes) === -1 && this.checked){
+        country_codes.push($(this).attr("data-code"));
       }
-      console.log(country_codes);
 
-    })
+      else if ($.inArray($(this).attr("data-code"), country_codes) >= 0 && !this.checked) {
+        country_codes.pop($(this).attr("data-code"));
+      }
 
+      else if ($.inArray($(this).attr("data-code"), countries)  ) {
 
-  }
+      }
+      console.log("This is country " +country_codes);
+      return country_codes;
+    }
+
+})
 
   function getDataPoint(){
     if(this.checked){
@@ -78,27 +66,6 @@ $(function(){
   }
 }
 
-function getCountryCode() {
-  let code ="";
-  country_arr.forEach(function(element,index){
-    if(country_arr.length === 1){
-       code  += countries[element]
-    }
-    else {
-      if(index === country_arr.length -1){
-        code += country_arr[element]
-      }
-      else {
-        code += country_arr[element]+";"
-      }
-
-    }
-
-  })
-  country = code;
-  console.log(country);
-  return code;
-}
 
 $.ajax({url:"assets/js/vendor/test.json"}).done(function(data){
 
