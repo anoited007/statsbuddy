120c14c357e34f4108b0e8608992cbdd4b094b7b
anoited007 <esackey15@alustudent.com>
2019-03-29 22:55:50 +0400

    chart init implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,8 +20,10 @@ var indicators = {"internet-penetration":"IT.NET.USER.P3","cellular-subscription
                   "fixed-broadband":"IT.NET.BBND.P2","secure-server":"IT.NET.SECR.P6", "personal-computers":"IT.CMP.PCMP.P2", "population":"SP.POP.TOTL", "population-density":"EN.POP.DNST",
                  "gdp":"NY.GDP.MKTP.CD", "gdp-percapita":"NY.GDP.PCAP.PP.KD"};
 
+var indicator = "IT.NET.USER.P3";
+
 var country_codes = ["GHA"];
-var country_code ; //will be initialiazed when ready to be used in getCountry function.
+var country_code = "GHA";
 
 //equivalent to $(document).ready()
 $(function(){
@@ -29,7 +31,7 @@ $(function(){
   //var canvas = $("#chart");
 
    // TODO: if country is more than 1 add ';' in between.
-  var indicator = "IT.NET.USER.P3";
+
 
   $("input[type=radio]").each(function() {
     $(this).on("change",getDataPoint);
@@ -83,17 +85,18 @@ $(function(){
 })
 
 $.ajax({
-  url:"https://api.worldbank.org/v2/country/GBR;GHA/indicators/IT.CEL.SETS.P2?date=2013:2018&format=json",
-  //url:baseUrl+"countries/"+country_code+"indicators/"+indicator+"?date=2013:2018&format=json",
+  //let url = baseUrl+"countries/"+country_code+"indicators/"+indicator+"?date=2013:2018&format=json"
+  //url:"https://api.worldbank.org/v2/country/GBR;GHA/indicators/IT.CEL.SETS.P2?date=2013:2018&format=json",
+  url: baseUrl.concat("countries/",country_code,"/indicators/",indicator,"?date=2013:2018&format=json"),
   crossDomain: true
-}
-).done(function(data){
+}).done(function(data){
   var canvas = $("#chart");
   let indicatorName;
   let labels = []; // labels are the names of the countries in the graph
   let dates = [];
   let values = [];
 
+if(data[1] != null){
   indicatorName = data[1][0]["indicator"]["value"];
   console.log(indicatorName);
 
@@ -117,26 +120,238 @@ $.ajax({
     console.log(values);
 
 })
-let element =$("input[type=radio]").filter(function(){return this.checked}).attr("value")
- new Chart(canvas,{
-  type: 'bar',
-  data: {
-    labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
-    datasets: [
-      {
-        label: "Population (millions)",
-        backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-        data: [2478,5267,734,784,433]
-      }
-    ]
-  },
-  options: {
-    legend: { display: false },
-    title: {
-      display: true,
-      text: 'Predicted world population (millions) in 2050'
+
+}
+
+
+let element = $("input[type=radio]").filter(function(){return this.checked}).attr("value");
+
+if(element === "secure-server" || element === "personal-computers"){
+
+    if(labels.length == 1){
+      new Chart(canvas, {
+        type: "bar",
+        data: {
+          labels: dates,
+          datasets: [{
+            label: labels[0],
+            data: values
+          }
+
+          ]
+        }
+      })
     }
+
+    else if (labels.length == 2){
+      let valueSet1 = []
+      let valueSet2 = []
+      let midValue = Math.round(labels.length / 2)
+
+      for(let i = 0, j = 0; i < values.length; i++){
+        if(i < midValue){
+          valueSet1[i] = labels[i];
+        }
+        else{
+          valueSet2[j++] = labels[i];
+        }
+        console.log(valueSet1);
+        console.log(valueSet2);
+
+      }
+      new Chart(canvas, {
+          type: 'bar',
+          data: {
+            labels: dates,
+            datasets: [
+              {
+                label: labels[0],
+                backgroundColor: "#3e95cd",
+                data: valueSet1
+              }, {
+                label: labels[1],
+                backgroundColor: "#8e5ea2",
+                data: valueSet2
+              }
+            ]
+          },
+          options: {
+            title: {
+              display: true,
+              text: indicatorName
+            }
+          }
+      });
+
+    }
+
+    else {
+      let valueSet1 = []
+      let valueSet2 = []
+      let valueSet3 = []
+      let midValue = Math.round(labels.length / 3)
+
+      for(let i = 0, j = 0; i < values.length; i++){
+        if(i < midValue) {
+          valueSet1[i] = labels[i];
+        }
+
+        else if (i >= midValue && i < values.length - midValue) {
+          valueSet2[i-midValue] = labels[i];
+        }
+
+        else{
+          valueSet3[j++] = labels[i];
+        }
+
+    }
+
+    new Chart(canvas, {
+        type: 'bar',
+        data: {
+          labels: dates,
+          datasets: [
+            {
+              label: labels[0],
+              backgroundColor: "#3e95cd",
+              data: valueSet1
+            }, {
+              label: labels[1],
+              backgroundColor: "#8e5ea2",
+              data: valueSet2
+            },
+            {
+              label: labels[2],
+              backgroundColor: "#8e5ea2",
+              data: valueSet3
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: indicatorName
+          }
+        }
+    });
+
+
   }
-})
+
+}
+
+else {
+
+  if(labels.length == 1){
+    new Chart(canvas, {
+      type: "line",
+      data: {
+        labels: dates,
+        datasets: [{
+          label: labels[0],
+          data: values
+        }
+
+        ]
+      }
+    })
+  }
+
+  else if (labels.length == 2){
+    let valueSet1 = []
+    let valueSet2 = []
+    let midValue = Math.round(labels.length / 2)
+
+    for(let i = 0, j = 0; i < values.length; i++){
+      if(i < midValue){
+        valueSet1[i] = labels[i];
+      }
+      else{
+        valueSet2[j++] = labels[i];
+      }
+      console.log(valueSet1);
+      console.log(valueSet2);
+
+    }
+    new Chart(canvas, {
+        type: 'line',
+        data: {
+          labels: dates,
+          datasets: [
+            {
+              label: labels[0],
+              backgroundColor: "#3e95cd",
+              data: valueSet1
+            }, {
+              label: labels[1],
+              backgroundColor: "#8e5ea2",
+              data: valueSet2
+            }
+          ]
+        },
+        options: {
+          title: {
+            display: true,
+            text: indicatorName
+          }
+        }
+    });
+
+  }
+
+  else {
+    let valueSet1 = []
+    let valueSet2 = []
+    let valueSet3 = []
+    let midValue = Math.round(labels.length / 3)
+
+    for(let i = 0, j = 0; i < values.length; i++){
+      if(i < midValue) {
+        valueSet1[i] = labels[i];
+      }
+
+      else if (i >= midValue && i < values.length - midValue) {
+        valueSet2[i-midValue] = labels[i];
+      }
+
+      else{
+        valueSet3[j++] = labels[i];
+      }
+
+  }
+
+  new Chart(canvas, {
+      type: 'line',
+      data: {
+        labels: dates,
+        datasets: [
+          {
+            label: labels[0],
+            backgroundColor: "#3e95cd",
+            data: valueSet1
+          }, {
+            label: labels[1],
+            backgroundColor: "#8e5ea2",
+            data: valueSet2
+          },
+          {
+            label: labels[2],
+            backgroundColor: "#8e5ea2",
+            data: valueSet3
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: indicatorName
+        }
+      }
+  });
+
+
+}
+
+}
 
 })
