65988eecb28edb8e2f1d17adfb3147ec614e725a
anoited007 <esackey15@alustudent.com>
2019-03-31 01:18:30 +0400

    countries.html set up

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 /*
-The first few lines get the footer and the header and inserts into the DOM
+Get the footer and the header and inserts into the DOM
 */
 $.get("assets/templates/header.html", function(data, status){
    $(".header-container").html(data);
@@ -9,349 +9,59 @@ $.get("assets/templates/footer.html", function(data, status){
    $("footer").html(data);
 })
 
-/*
-Declaring Global Variables. These are Global because they will be accessed in different scopes of the script.
-Also, they don't require the DOM to be ready before they can be initialiazed.
-*/
-let baseUrl = "https://api.worldbank.org/v2/";
-
-//var countries = {"ghana":"GHA","mauritius":"MUS", "united-kingdom":"GBR" };
-var indicators = {"internet-penetration":"IT.NET.USER.P3","cellular-subscription":"IT.CEL.SETS.P2",
-                  "fixed-broadband":"IT.NET.BBND.P2","secure-server":"IT.NET.SECR.P6", "personal-computers":"IT.CMP.PCMP.P2", "population":"SP.POP.TOTL", "population-density":"EN.POP.DNST",
-                 "gdp":"NY.GDP.MKTP.CD", "gdp-percapita":"NY.GDP.PCAP.PP.KD"};
-
-var indicator = "IT.NET.USER.P3";
-
-var country_codes = ["GHA"];
-var country_code = "GHA";
-
-//equivalent to $(document).ready()
-$(function(){
-  var countries = $("input[type=checkbox]");
-  //var canvas = $("#chart");
-
-   // TODO: if country is more than 1 add ';' in between.
-
-
-  $("input[type=radio]").each(function() {
-    $(this).on("change",getDataPoint);
-  })
-
-  $("input[type=checkbox]").each(function() {
-    $(this).on("change",getCountry);
-  })
-
-
-    function getCountry(){
-      // Saving the this object in the current context to use it when the contenxt of this changes.
-      let element = $(this).attr("data-code");
-      let index = country_codes.indexOf(element);
-      country_code = ""; //initialiazed to be used in loop after getting codes.
-
-      if( index === -1 && this.checked){
-        country_codes.push(element);
-      }
-
-      else if (index >= 0 && !this.checked) {
-        country_codes.splice(index, 1);
-      }
-
-    for (let i = 0; i < country_codes.length; i++) {
-
-      if(i === country_codes.length -1){
-        country_code += country_codes[i]
-      }
-      else {
-        country_code += country_codes[i] +";"
-      }
-
-    }
-
-
-      console.log("This is country " +country_codes);
-      console.log(country_code);
-      return country_code;
-      }
-
-  function getDataPoint(){
-    if(this.checked){
-      indicator = indicators[this.value]
-      console.log(this.value);
-      console.log(indicator);
-      return this.value;
-  }
- }
-
-})
-
-$.ajax({
-  //let url = baseUrl+"countries/"+country_code+"indicators/"+indicator+"?date=2013:2018&format=json"
-  //url:"https://api.worldbank.org/v2/country/GBR;GHA/indicators/IT.CEL.SETS.P2?date=2013:2018&format=json",
-  url: baseUrl.concat("countries/",country_code,"/indicators/",indicator,"?date=2013:2018&format=json"),
-  crossDomain: true
-}).done(function(data){
-  var canvas = $("#chart");
-  let indicatorName;
-  let labels = []; // labels are the names of the countries in the graph
-  let dates = [];
-  let values = [];
-
-if(data[1] != null){
-  indicatorName = data[1][0]["indicator"]["value"];
-  console.log(indicatorName);
-
-  $.each(data[1],function (index, data) {
-    let date = data["date"];
-
-    if($.inArray(date,dates)=== -1){
-      dates.push(date);
-    }
-
-    values.push(Math.round(this["value"]))
-    let country_name = data.country.value;
-
-// if country name hasn't been added to labels, add it
-    if($.inArray(country_name,labels) === -1){
-      labels.push(country_name);
-    }
-    console.log("printing current items");
-    console.log(labels);
-    console.log(dates);
-    console.log(values);
-
-})
-
-}
-
-
-let element = $("input[type=radio]").filter(function(){return this.checked}).attr("value");
-
-if(element === "secure-server" || element === "personal-computers"){
-
-    if(labels.length == 1){
-      new Chart(canvas, {
-        type: "bar",
-        data: {
-          labels: dates,
-          datasets: [{
-            label: labels[0],
-            data: values
-          }
-
-          ]
-        }
-      })
-    }
-
-    else if (labels.length == 2){
-      let valueSet1 = []
-      let valueSet2 = []
-      let midValue = Math.round(labels.length / 2)
-
-      for(let i = 0, j = 0; i < values.length; i++){
-        if(i < midValue){
-          valueSet1[i] = labels[i];
-        }
-        else{
-          valueSet2[j++] = labels[i];
-        }
-        console.log(valueSet1);
-        console.log(valueSet2);
-
-      }
-      new Chart(canvas, {
-          type: 'bar',
-          data: {
-            labels: dates,
-            datasets: [
-              {
-                label: labels[0],
-                backgroundColor: "#3e95cd",
-                data: valueSet1
-              }, {
-                label: labels[1],
-                backgroundColor: "#8e5ea2",
-                data: valueSet2
-              }
-            ]
-          },
-          options: {
-            title: {
-              display: true,
-              text: indicatorName
-            }
-          }
-      });
-
-    }
-
-    else {
-      let valueSet1 = []
-      let valueSet2 = []
-      let valueSet3 = []
-      let midValue = Math.round(labels.length / 3)
-
-      for(let i = 0, j = 0; i < values.length; i++){
-        if(i < midValue) {
-          valueSet1[i] = labels[i];
-        }
-
-        else if (i >= midValue && i < values.length - midValue) {
-          valueSet2[i-midValue] = labels[i];
-        }
-
-        else{
-          valueSet3[j++] = labels[i];
-        }
-
-    }
-
-    new Chart(canvas, {
-        type: 'bar',
-        data: {
-          labels: dates,
-          datasets: [
-            {
-              label: labels[0],
-              backgroundColor: "#3e95cd",
-              data: valueSet1
-            }, {
-              label: labels[1],
-              backgroundColor: "#8e5ea2",
-              data: valueSet2
-            },
-            {
-              label: labels[2],
-              backgroundColor: "#8e5ea2",
-              data: valueSet3
-            }
-          ]
-        },
-        options: {
-          title: {
-            display: true,
-            text: indicatorName
-          }
-        }
-    });
-
-
-  }
-
-}
-
-else {
-
-  if(labels.length == 1){
-    new Chart(canvas, {
-      type: "line",
-      data: {
-        labels: dates,
-        datasets: [{
-          label: labels[0],
-          data: values
-        }
-
-        ]
-      }
-    })
-  }
-
-  else if (labels.length == 2){
-    let valueSet1 = []
-    let valueSet2 = []
-    let midValue = Math.round(labels.length / 2)
-
-    for(let i = 0, j = 0; i < values.length; i++){
-      if(i < midValue){
-        valueSet1[i] = labels[i];
-      }
-      else{
-        valueSet2[j++] = labels[i];
-      }
-      console.log(valueSet1);
-      console.log(valueSet2);
-
-    }
-    new Chart(canvas, {
-        type: 'line',
-        data: {
-          labels: dates,
-          datasets: [
-            {
-              label: labels[0],
-              backgroundColor: "#3e95cd",
-              data: valueSet1
-            }, {
-              label: labels[1],
-              backgroundColor: "#8e5ea2",
-              data: valueSet2
-            }
-          ]
-        },
-        options: {
-          title: {
-            display: true,
-            text: indicatorName
-          }
-        }
+$(function () {
+  if (document.querySelector(".mln_sec")) {
+    /* Initialiaze scrolling plugin */
+    $('body').mln_full({
+      'target' : '.mln_sec',
+      'color' : '#455a64',
+      'hover' : '#607d8b',
+      'full' : true,
+      'itens' : ['Ghana', 'Mauritius', 'United Kingdom']
     });
 
   }
 
-  else {
-    let valueSet1 = []
-    let valueSet2 = []
-    let valueSet3 = []
-    let midValue = Math.round(labels.length / 3)
+})
 
-    for(let i = 0, j = 0; i < values.length; i++){
-      if(i < midValue) {
-        valueSet1[i] = labels[i];
-      }
+/* Initialising Google Maps */
+function initMap() {
+  var ghana = {
+    lat: 8.0300284,
+    lng: -1.0800271
+  };
 
-      else if (i >= midValue && i < values.length - midValue) {
-        valueSet2[i-midValue] = labels[i];
-      }
+  var mauritius = {
+    lat: -20.2759451,
+    lng: 57.5703566
+  };
 
-      else{
-        valueSet3[j++] = labels[i];
-      }
+  var uk = {
+    lat: 54.7023545,
+    lng: -3.2765753
+  };
 
-  }
+  var ghMap = new google.maps.Map(document.getElementById("ghana"), {
+    zoom: 15,
+    center: ghana
+  });
+/* Using single quotes to escape the double quotes */
+  var ghDetails = '<div id="map-content"><p id="location-title">Cim Finance</p> <i class="fa fa-map-marker" aria-hidden="true"></i> Manhattan Building, Edith Cavell St, Port Louis <br /> <i class="fa fa-globe" aria-hidden="true"></i><a href="http://cimfinance.mu">Visit Our Website </a></div>';
 
-  new Chart(canvas, {
-      type: 'line',
-      data: {
-        labels: dates,
-        datasets: [
-          {
-            label: labels[0],
-            backgroundColor: "#3e95cd",
-            data: valueSet1
-          }, {
-            label: labels[1],
-            backgroundColor: "#8e5ea2",
-            data: valueSet2
-          },
-          {
-            label: labels[2],
-            backgroundColor: "#8e5ea2",
-            data: valueSet3
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: indicatorName
-        }
-      }
+  var murDetails = '<div id="map-content"><p id="location-title">Cim Finance</p> <i class="fa fa-map-marker" aria-hidden="true"></i> Manhattan Building, Edith Cavell St, Port Louis <br /> <i class="fa fa-globe" aria-hidden="true"></i><a href="http://cimfinance.mu">Visit Our Website </a></div>';
+
+  var ukDetails = '<div id="map-content"><p id="location-title">Cim Finance</p> <i class="fa fa-map-marker" aria-hidden="true"></i> Manhattan Building, Edith Cavell St, Port Louis <br /> <i class="fa fa-globe" aria-hidden="true"></i><a href="http://cimfinance.mu">Visit Our Website </a></div>';
+
+  var ghWindow = new google.maps.ghWindow({
+    content: ghDetails
   });
 
-
+  var ghMarker = new google.maps.ghMarker({
+    position: ghana,
+    map: ghMap,
+    title: "Ghana"
+  });
+  ghMarker.addListener("click", function() {
+    ghWindow.open(ghMap, marker);
+  });
 }
-
-}
-
-})
